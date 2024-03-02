7ace7955c27a6f527e97adb5d6f607abd742b871
Mohammed Sarfaraz <>
2024-03-02 21:27:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,179 @@
+const ProductobjTop10 = [
+    {
+      id: 1,
+      name: "Aggricultural land",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/10tVl11u1yJa4jC_LmdFMOaGyft9774fH=w1000",
+      ],
+      property: "Nakre Agricultural property",
+      price: "20000 per cents",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of Karkala, this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 2,
+      name: "Aggricultural land with Farm",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/1517Hx4O5Ngnp8mNXfIDgOwMdg01pYW1V",
+      ],
+      property: "Belman Agricultural property",
+      price: "1.5cr",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of [Location], this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 3,
+      name: "Nakubeedi  land available for sale",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/15K2HCklYLZZSmH8s61ucMTfQC2RAN6bb",
+      ],
+      property: "Land",
+      price: "20000 per cent ",
+      description: ` land available for sale 
+      Nakubeedi ninjoor, Modubelle road
+      10 acre land`,
+    },
+  ];
+  const Productobj = [
+    {
+      id: 1,
+      name: "Aggricultural land",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/10tVl11u1yJa4jC_LmdFMOaGyft9774fH=w1000",
+      ],
+      property: "Nakre Agricultural property",
+      price: "20000 per cents",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of Karkala, this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 2,
+      name: "Aggricultural land with Farm",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/1517Hx4O5Ngnp8mNXfIDgOwMdg01pYW1V",
+      ],
+      property: "Belman Agricultural property",
+      price: "1.5cr",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of [Location], this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 3,
+      name: "Nakubeedi  land available for sale",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/15K2HCklYLZZSmH8s61ucMTfQC2RAN6bb",
+      ],
+      property: "Land",
+      price: "20000 per cent ",
+      description: ` land available for sale 
+      Nakubeedi ninjoor, Modubelle road
+      10 acre land`,
+    },
+    {
+      id: 1,
+      name: "Aggricultural land",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/10tVl11u1yJa4jC_LmdFMOaGyft9774fH=w1000",
+      ],
+      property: "Nakre Agricultural property",
+      price: "20000 per cents",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of Karkala, this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 2,
+      name: "Aggricultural land with Farm",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/1517Hx4O5Ngnp8mNXfIDgOwMdg01pYW1V",
+      ],
+      property: "Belman Agricultural property",
+      price: "1.5cr",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of [Location], this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 3,
+      name: "Nakubeedi  land available for sale",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/15K2HCklYLZZSmH8s61ucMTfQC2RAN6bb",
+      ],
+      property: "Land",
+      price: "20000 per cent ",
+      description: ` land available for sale 
+      Nakubeedi ninjoor, Modubelle road
+      10 acre land`,
+    },
+    {
+      id: 1,
+      name: "Aggricultural land",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/10tVl11u1yJa4jC_LmdFMOaGyft9774fH=w1000",
+      ],
+      property: "Nakre Agricultural property",
+      price: "20000 per cents",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of Karkala, this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 2,
+      name: "Aggricultural land with Farm",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/1517Hx4O5Ngnp8mNXfIDgOwMdg01pYW1V",
+      ],
+      property: "Belman Agricultural property",
+      price: "1.5cr",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of [Location], this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 3,
+      name: "Nakubeedi  land available for sale",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/15K2HCklYLZZSmH8s61ucMTfQC2RAN6bb",
+      ],
+      property: "Land",
+      price: "20000 per cent ",
+      description: ` land available for sale 
+      Nakubeedi ninjoor, Modubelle road
+      10 acre land`,
+    },
+    {
+      id: 1,
+      name: "Aggricultural land",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/10tVl11u1yJa4jC_LmdFMOaGyft9774fH=w1000",
+      ],
+      property: "Nakre Agricultural property",
+      price: "20000 per cents",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of Karkala, this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 2,
+      name: "Aggricultural land with Farm",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/1517Hx4O5Ngnp8mNXfIDgOwMdg01pYW1V",
+      ],
+      property: "Belman Agricultural property",
+      price: "1.5cr",
+      description: ` Discover your opportunity to own a piece of lush agricultural land, rich with vibrant arecanut and coconut tree plantations. Nestled in the heart of [Location], this fertile land offers an ideal setting for agricultural enthusiasts and investors alike..`,
+    },
+    {
+      id: 3,
+      name: "Nakubeedi  land available for sale",
+      category: "Land ",
+      imagess: [
+        " https://lh3.googleusercontent.com/d/15K2HCklYLZZSmH8s61ucMTfQC2RAN6bb",
+      ],
+      property: "Land",
+      price: "20000 per cent ",
+      description: ` land available for sale 
+      Nakubeedi ninjoor, Modubelle road
+      10 acre land`,
+    },
+  ];
